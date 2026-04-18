a81e2f388631a8b3ebff0e0afd4b5ce0998d2ea2
Gowtham Surya L <suryastars119@gmail.com>
2026-04-18 10:02:41 +0530

    Add files via upload
    commit done files uploaded

---
+++ 1.js
@@ -0,0 +1,8 @@
+var Eid=1001
+let Ename="Gowtham"
+const Esal=29000
+document.write("Employee_Id is:"+Eid)
+document.write("<hr>")
+doucment.write("Employee_Name is:"+Ename)
+document.write("<hr>")
+document.write("Esal is:"+Esal)
